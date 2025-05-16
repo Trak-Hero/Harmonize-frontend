2130b93c547536c0d9e132cbedcc9d977beda4a4
Rachael <156548776+rachaelhuang@users.noreply.github.com>
2025-05-16 02:50:36 -0400

    added more directories & navbar component

--- reverberate-client/src/App.jsx
+++ reverberate-client/src/App.jsx
@@ -1,9 +1,13 @@
+import Navbar from './components/navbar';
 import './App.css'
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen text-4xl font-bold text-blue-500">
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center h-screen text-4xl font-bold text-blue-500">
       Hello from Reverberate
+    </div>
     </div>
   );
 }
---
+++ reverberate-client/src/components/navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-left">
+        <a href="/" className="nav-logo">Logo goes here</a>
+        <a href="/" className="nav-link">Home</a>
+        <a href="/discover" className="nav-link">Discover</a>
+      </div>
+
+      <div className="nav-search">
+        <input
+          type="text"
+          placeholder="Search artists, songs, or events"
+          className="search-input"
+        />
+      </div>
+
+      <div className="nav-right">
+        <a href="/profile" className="nav-link">Profile</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
