d812a90f2840fd329e73d443dc657385bc1fd477
Purin (Trak) Prateepmanowong <purin.trak@gmail.com>
2025-06-01 16:50:47 -0400

    fix 16

--- src/utils/mapTrack.js
+++ src/utils/mapTrack.js
@@ -1,13 +1,14 @@
 // src/utils/mapTrack.js
-
-export default function mapTrack(track) {
+export default function mapTrack(track = {}) {
   return {
-    name: track.name || track.title || 'Untitled',
+    id:      track.id || crypto.randomUUID(),
+    name:    track.name || track.title || 'Untitled',
     artists: track.artists?.map(a => a.name) || [track.artist || 'Unknown'],
     image:
       track.album?.images?.[0]?.url ||
       track.cover ||
       track.image ||
       '/fallback.jpg',
+    preview: track.preview_url || track.preview || null,
   };
 }
